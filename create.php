040fa0776db5e95999fda2bf210035f0d497a3f7
stanis <stanis@netvolution.fr>
2024-02-23 10:17:25 +0100

    Ajout de page create.php

---
+++ create.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Créer un utilisateur</title>
+    <link rel="stylesheet" href="css/pico.css">
+</head>
+<body>
+<div class="container">
+    <h1>Créer un utilisateur</h1>
+    <form>
+        <label for="name">Nom</label>
+        <input type="text" id="name" name="name">
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
+    </form>
+</div>
+</body>
+</html>
